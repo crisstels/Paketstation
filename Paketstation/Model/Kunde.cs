7aeccbb48a5f49c894b1406e1ee72091eb21395f
crisstels <natalie-hasselmann@web.de>
2020-10-28 11:36:43 +0100

    added properties for Kunde and Paket

---
+++ Paketstation/Model/Kunde.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paketstation.Model
+{
+    class Kunde
+    {
+        #region Properties
+        private Paket _paket;
+        private int _kundennummer;
+        private String _name;
+        private String _adresse;
+        #endregion
+
+        #region Accessors/Modifiers
+        public int Kundennummer { get => _kundennummer; set => _kundennummer = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Adresse { get => _adresse; set => _adresse = value; }
+        public Paket Paket { get => _paket; set => _paket = value; }
+        #endregion
+    }
+}
---
+++ Paketstation/Model/Paket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paketstation.Model
+{
+    class Paket
+    {
+        #region Properties
+        private String _paketschein;
+        private int _gewicht;
+        private String _groesse;
+        private String _empfaenger;
+        private String _sender;
+        #endregion
+
+        #region Accessors/Modifiers
+        public string Paketschein { get => _paketschein; set => _paketschein = value; }
+        public int Gewicht { get => _gewicht; set => _gewicht = value; }
+        public string Groesse { get => _groesse; set => _groesse = value; }
+        public string Empfaenger { get => _empfaenger; set => _empfaenger = value; }
+        public string Sender { get => _sender; set => _sender = value; }
+        #endregion
+    }
+}
