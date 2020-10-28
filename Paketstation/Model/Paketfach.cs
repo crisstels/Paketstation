2481ec76a6f19cbe6a06cb1b035419805805c757
crisstels <natalie-hasselmann@web.de>
2020-10-28 11:46:07 +0100

    added Properties for Paket, Paketfach and Paketstation

--- Paketstation/Model/Paketfach.cs
+++ Paketstation/Model/Paketfach.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Paketstation.Model
-{
+namespace Paketstation
     class Paketfach
     {
+        #region Properties
+        private Paket _inhalt;
+        private int _nummer;
+        private bool _status;
+        private bool _belegt;
+        #endregion
+
+        #region Accessors/Modifiers
+        public int Nummer { get => _nummer; set => _nummer = value; }
+        public bool Status { get => _status; set => _status = value; }
+        public bool Belegt { get => _belegt; set => _belegt = value; }
+        internal Paket Inhalt { get => _inhalt; set => _inhalt = value; }
+        #endregion
+
     }
 }
--- Paketstation/Model/Paketstation.cs
+++ Paketstation/Model/Paketstation.cs
@@ -8,5 +8,18 @@ namespace Paketstation
 {
     class Paketstation
     {
+        #region Properties
+        private Paketfach[] _faecher = new Paketfach[9];
+        private UserInterface _terminal;
+        private String _standort;
+        private int ID;
+        #endregion
+
+        #region Accessors/Modifiers
+        public int ID1 { get => ID; set => ID = value; }
+        public string Standort { get => _standort; set => _standort = value; }
+        internal UserInterface Terminal { get => _terminal; set => _terminal = value; }
+        internal Paketfach[] Faecher { get => _faecher; set => _faecher = value; }
+        #endregion
     }
 }
